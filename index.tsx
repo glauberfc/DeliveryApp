d9e4490ec4fc160d18943dac94fd379d6fabe4a0
Glauber Castro <gfc714@gmail.com>
2020-04-17 00:33:56 -0300

    Start to work with React Navigation

--- index.tsx
+++ index.tsx
@@ -1,8 +1,53 @@
-import { registerRootComponent } from 'expo';
+import 'react-native-gesture-handler'
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { registerRootComponent } from 'expo'
+import { createStackNavigator } from '@react-navigation/stack'
+import { View, Text, Button } from 'react-native'
 
-import App from './App';
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  )
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  )
+}
+
+const Stack = createStackNavigator()
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Title  ' }}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(App)
